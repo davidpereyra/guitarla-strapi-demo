df087fab1230adfd4d41565033d30586658a6978
David Pereyra <webdeveloperdavid@gmail.com>
2023-01-16 09:16:33 -0300

    +1 db

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ module.exports = ({ env }) => ({
     },
   },
 });
-*/
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
@@ -22,6 +22,21 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'strapi'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', '2412'),
+      ssl: env.bool('DATABASE_SSL', false),
+    },
+  },
+});
+*/
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'guitarla'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', 'Dv1d'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
